9439ec8e09d00e89d4b1d2d9803b4dc6d5367244
AmadeoBon <104178969+Amadeo-Frontend@users.noreply.github.com>
2022-10-22 16:31:34 -0300

    Add files via upload

---
+++ mod-26-css/src/js/index.js
@@ -0,0 +1,17 @@
+import '../scss/styles.scss'
+/*import imageLogo from '../assets/images/logo-w.png'
+
+const elemRoot = document.getElementById('root')
+elemRoot.classList.add('container')
+
+const elemImageBuilding = new Image()
+elemImageBuilding.src = imageLogo
+
+const elemImageNotebook = new Image()
+elemImageNotebook.src = imageNotebook
+
+document.querySelector('#logo')
+    .appendChild(elemImageBuilding)
+
+document.querySelector('#notebook')
+    .appendChild(elemImageNotebook)*/
